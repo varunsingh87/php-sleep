1825066821c9faf83217cae5bcc1d28e58b17de6
Varun <31084854+Borumer@users.noreply.github.com>
2020-11-01 15:43:23 -0500

    Implement UserKnownHandler (Transferred)

---
+++ src/DBHandlers/UserKnownHandler.php
@@ -0,0 +1,55 @@
+<?php 
+
+namespace VarunS\BorumSleep\DBHandlers;
+
+class UserKnownHandler extends DBHandler {
+    /**
+     * The result of the MySQL query to get the user with the passed in api key
+     * @var mysqli_result 
+     */
+    protected $user;
+
+    /**
+     * The associative array on the user or lack thereof who is querying the notes
+     * @var Array
+     */
+    protected $userArray;
+
+    /**
+     * The id of the user
+     * @var int
+     */
+    protected $userId;
+    
+    /**
+     * @param string $userApiKey The api key of the user requesting the jottings
+     */
+    function __construct($userApiKey) {
+        parent::__construct();
+
+        // Exit early if the API key is invalid
+        $this->user = $this->getUserFromApiKey($userApiKey);
+        $this->handleInvalidApiKey($this->user);
+        $this->userArray = mysqli_fetch_array($this->user, MYSQLI_BOTH);
+        $this->userId = $this->userArray['borum_user_id'];
+    }
+
+    /** 
+     * Helper logic that exits the script if the user is invalid
+     * @param mysqli_result $user The result of the query that selects the user with the passed in api key
+     * @return void 
+    */
+    protected function handleInvalidApiKey($user) : void {
+        if (mysqli_num_rows($user) == 0) {
+            echo json_encode([
+                "statusCode" => 404,
+                "error" => [
+                    "message" => "The user does not exist"
+                ]
+            ]);
+            exit();
+        }
+    }
+}
+
+?>
