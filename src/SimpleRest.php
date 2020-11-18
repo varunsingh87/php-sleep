51aa473fb4bc3576715110197784ba7683f53d69
Varun <31084854+Borumer@users.noreply.github.com>
2020-11-17 20:43:45 -0500

    Add headers to setHttpHeaders Adds support for CORS

--- src/SimpleRest.php
+++ src/SimpleRest.php
@@ -1,12 +1,13 @@
-<?php 
+<?php
 
 namespace VarunS\BorumSleep;
 
 /**
-* A simple RESTful webservices base class
-*/
-class SimpleRest {
-	
+ * A simple RESTful webservices base class
+ */
+class SimpleRest
+{
+
 	private static $httpVersion = "HTTP/1.1";
 
 	/**
@@ -14,66 +15,74 @@ class SimpleRest {
 	 * @param int|string $statusCode The status code that will be passed into SimpleRest::getHttpStatusMessage
 	 * @param string $contentType The Content-Type header value, defaults to application/json
 	 */
-	public static function setHttpHeaders($statusCode, $contentType = NULL) {
+	public static function setHttpHeaders($statusCode, $contentType = NULL)
+	{
 		$statusMessage = SimpleRest::getHttpStatusMessage($statusCode);
-		
-		header(SimpleRest::$httpVersion. " " . $statusCode ." ". $statusMessage);		
-		header("Content-Type:". ($contentType ?? "application/json") . "; charset=UTF-8");
-		header("Access-Control-Allow-Origin: *");
+
+		header(SimpleRest::$httpVersion . " " . $statusCode . " " . $statusMessage);
+		header("Content-Type:" . ($contentType ?? "application/json") . "; charset=UTF-8");
+
+		// Needed for CORS Options request
+		header('Access-Control-Expose-Headers: Authorization,authorization');
+		header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN'] . '');
+		header('Access-Control-Allow-Credentials: true');
 	}
-	
+
 	/**
 	 * Gets the standard Http status message that corresponds to the passed in status code
 	 * @param int|string $statusCode
 	 * @return string The status message
 	 */
-	public static function getHttpStatusMessage($statusCode){
+	public static function getHttpStatusMessage($statusCode)
+	{
 		$httpStatus = array(
-			100 => 'Continue',  
-			101 => 'Switching Protocols',  
+			100 => 'Continue',
+			101 => 'Switching Protocols',
 			200 => 'OK',
-			201 => 'Created',  
-			202 => 'Accepted',  
-			203 => 'Non-Authoritative Information',  
-			204 => 'No Content',  
-			205 => 'Reset Content',  
-			206 => 'Partial Content',  
-			300 => 'Multiple Choices',  
-			301 => 'Moved Permanently',  
-			302 => 'Found',  
-			303 => 'See Other',  
-			304 => 'Not Modified',  
-			305 => 'Use Proxy',  
-			306 => '(Unused)',  
-			307 => 'Temporary Redirect',  
-			400 => 'Bad Request',  
-			401 => 'Unauthorized',  
-			402 => 'Payment Required',  
-			403 => 'Forbidden',  
-			404 => 'Not Found',  
-			405 => 'Method Not Allowed',  
-			406 => 'Not Acceptable',  
-			407 => 'Proxy Authentication Required',  
-			408 => 'Request Timeout',  
-			409 => 'Conflict',  
-			410 => 'Gone',  
-			411 => 'Length Required',  
-			412 => 'Precondition Failed',  
-			413 => 'Request Entity Too Large',  
-			414 => 'Request-URI Too Long',  
-			415 => 'Unsupported Media Type',  
-			416 => 'Requested Range Not Satisfiable',  
-			417 => 'Expectation Failed',  
-			500 => 'Internal Server Error',  
-			501 => 'Not Implemented',  
-			502 => 'Bad Gateway',  
-			503 => 'Service Unavailable',  
-			504 => 'Gateway Timeout',  
-			505 => 'HTTP Version Not Supported');
+			201 => 'Created',
+			202 => 'Accepted',
+			203 => 'Non-Authoritative Information',
+			204 => 'No Content',
+			205 => 'Reset Content',
+			206 => 'Partial Content',
+			300 => 'Multiple Choices',
+			301 => 'Moved Permanently',
+			302 => 'Found',
+			303 => 'See Other',
+			304 => 'Not Modified',
+			305 => 'Use Proxy',
+			306 => '(Unused)',
+			307 => 'Temporary Redirect',
+			400 => 'Bad Request',
+			401 => 'Unauthorized',
+			402 => 'Payment Required',
+			403 => 'Forbidden',
+			404 => 'Not Found',
+			405 => 'Method Not Allowed',
+			406 => 'Not Acceptable',
+			407 => 'Proxy Authentication Required',
+			408 => 'Request Timeout',
+			409 => 'Conflict',
+			410 => 'Gone',
+			411 => 'Length Required',
+			412 => 'Precondition Failed',
+			413 => 'Request Entity Too Large',
+			414 => 'Request-URI Too Long',
+			415 => 'Unsupported Media Type',
+			416 => 'Requested Range Not Satisfiable',
+			417 => 'Expectation Failed',
+			500 => 'Internal Server Error',
+			501 => 'Not Implemented',
+			502 => 'Bad Gateway',
+			503 => 'Service Unavailable',
+			504 => 'Gateway Timeout',
+			505 => 'HTTP Version Not Supported'
+		);
 		return $httpStatus[$statusCode] ?? $httpStatus[500];
 	}
 
-	public static function handleNoApiKey() {
+	public static function handleNoApiKey()
+	{
 		if (!isset($_GET['app_api_key'])) {
 			SimpleRest::setHttpHeaders(403); // Set header to forbidden error
 			echo json_encode([
@@ -89,26 +98,28 @@ class SimpleRest {
 	 * Handle the validation of a serverless function's post parameters
 	 * @param Array $requiredParameters The required POST parameters
 	 */
-	public static function handlePostParameterValidation(String... $requiredParameters) {
+	public static function handlePostParameterValidation(String ...$requiredParameters)
+	{
 		$errors = array();
 		foreach ($requiredParameters as $paramName) {
-            if (!isset($_POST[$paramName])) {
-                array_push($errors, $paramName);
-            }
-        }
-        if (sizeof($errors) > 0) {
-            SimpleRest::setHttpHeaders(400);
-            echo json_encode([
-                "error" => [
-                    "message" => "The required POST parameters were not provided",
-                    "missingParameters" => $errors
-                ]
-            ]);
-            exit();
-        }
+			if (!isset($_POST[$paramName])) {
+				array_push($errors, $paramName);
+			}
+		}
+		if (sizeof($errors) > 0) {
+			SimpleRest::setHttpHeaders(400);
+			echo json_encode([
+				"error" => [
+					"message" => "The required POST parameters were not provided",
+					"missingParameters" => $errors
+				]
+			]);
+			exit();
+		}
 	}
 
-	public static function handleDeleteParameterValidation(String... $requiredParameters) {
+	public static function handleDeleteParameterValidation(String ...$requiredParameters)
+	{
 		$errors = array();
 		foreach ($requiredParameters as $paramName) {
 			if (!isset($GLOBALS["_{DELETE}"][$paramName])) {
@@ -128,7 +139,8 @@ class SimpleRest {
 		}
 	}
 
-	public static function handleHeaderValidation($headers, String... $requiredHeaders) {
+	public static function handleHeaderValidation($headers, String ...$requiredHeaders)
+	{
 		$errors = array();
 
 		foreach ($requiredHeaders as $header) {
@@ -137,23 +149,24 @@ class SimpleRest {
 			}
 		}
 
-        if (sizeof($errors) > 0) {
-            SimpleRest::setHttpHeaders(401);
-            echo json_encode([
-                "error" => [
+		if (sizeof($errors) > 0) {
+			SimpleRest::setHttpHeaders(401);
+			echo json_encode([
+				"error" => [
 					"message" => "Access not authorized without proper headers",
 					"missingHeaders" => $errors
-                ]
-            ]);
-            exit();
-        }
+				]
+			]);
+			exit();
+		}
 	}
 
 	/**
 	 * Validates and echos an error with status code 405 when an invalid request method is used
 	 * @param Array $validRequestMethods The allowed request methods
 	 */
-	public static function handleRequestMethodValidation(String... $validRequestMethods) {
+	public static function handleRequestMethodValidation(String ...$validRequestMethods)
+	{
 		header("Access-Control-Allow-Methods: " . join(", ", $validRequestMethods));
 		if (!in_array($_SERVER['REQUEST_METHOD'], $validRequestMethods)) {
 			SimpleRest::setHttpHeaders(405);
@@ -167,34 +180,35 @@ class SimpleRest {
 		}
 	}
 
-	
-    /**
-     * Returns a 400 Bad Request error if the passed in id is not numeric
-     * @param int $id The id of the item
-     * @return Array statusCode 400 if not numeric and status code 100 if it is
-     */
-    public static function handleNonNumericId($id, $prefix="") {
-        if (!is_numeric($id)) {
-            return [
-                "statusCode" => 400,
-                "error" => [
-                    "message" => 'The ' . $prefix . 'id must be numeric'
-                ]
-            ];
-        } else {
-            return [
-                "statusCode" => 100
-            ];
-        }
+
+	/**
+	 * Returns a 400 Bad Request error if the passed in id is not numeric
+	 * @param int $id The id of the item
+	 * @return Array statusCode 400 if not numeric and status code 100 if it is
+	 */
+	public static function handleNonNumericId($id, $prefix = "")
+	{
+		if (!is_numeric($id)) {
+			return [
+				"statusCode" => 400,
+				"error" => [
+					"message" => 'The ' . $prefix . 'id must be numeric'
+				]
+			];
+		} else {
+			return [
+				"statusCode" => 100
+			];
+		}
 	}
-	
+
 	/**
 	 * Gets the usable value from the authorization header
 	 * @param string $authorizationHeader The full string
 	 * @return string The value after the text "Basic "
 	 */
-	public static function parseAuthorizationHeader($authorizationHeader) : String {
+	public static function parseAuthorizationHeader($authorizationHeader): String
+	{
 		return substr($authorizationHeader, strlen("Basic "));
 	}
 }
-?>
