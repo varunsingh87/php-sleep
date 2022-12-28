fb7f77035ba73c7abc8b0b315456ff39d9cd5699
Varun <31084854+varunsingh87@users.noreply.github.com>
2022-12-28 15:26:51 -0500

    Add check for production environment

--- src/DotEnv.php
+++ src/DotEnv.php
@@ -6,8 +6,11 @@ class DotEnv
 {
     public static function loadIfLocal() {
         $path = $_SERVER['DOCUMENT_ROOT'] . '/.env';
-        if(!file_exists($path)) {
+        
+        if(!file_exists($path) && count($_ENV) < 0) {
             throw new \InvalidArgumentException(sprintf('%s does not exist', $path));
+        } else if (!file_exists($path)) {
+            return;
         }
 
         if (!is_readable($path)) {
@@ -17,18 +20,17 @@ class DotEnv
         $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
         foreach ($lines as $line) {
 
-            if (strpos(trim($line), '#') === 0) {
-                continue;
-            }
+            if (strpos(trim($line), '#') !== 0) {
 
-            list($name, $value) = explode('=', $line, 2);
-            $name = trim($name);
-            $value = trim($value);
+                list($name, $value) = explode('=', $line, 2);
+                $name = trim($name);
+                $value = trim($value);
 
-            if (!array_key_exists($name, $_SERVER) && !array_key_exists($name, $_ENV)) {
-                putenv(sprintf('%s=%s', $name, $value));
-                $_ENV[$name] = $value;
-                $_SERVER[$name] = $value;
+                if (!array_key_exists($name, $_SERVER) && !array_key_exists($name, $_ENV)) {
+                    putenv(sprintf('%s=%s', $name, $value));
+                    $_ENV[$name] = $value;
+                    $_SERVER[$name] = $value;
+                }
             }
         }
     }
